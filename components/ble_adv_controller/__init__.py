941631f8d02fd600425b0630e65821a474421aed
Cbrogg <73609563+Cbrogg@users.noreply.github.com>
2026-06-04 17:03:46 +0300

    Fix handler registration for ESPHome 2026.5

--- components/ble_adv_controller/__init__.py
+++ components/ble_adv_controller/__init__.py
@@ -4,9 +4,7 @@ from esphome.core import ID
 from esphome.const import (
     CONF_DURATION,
     CONF_ID,
-    CONF_NAME,
     CONF_REVERSED,
-    CONF_TYPE,
     CONF_INDEX,
     CONF_VARIANT,
     PLATFORM_ESP32,
@@ -34,39 +32,28 @@ BleAdvEntity = bleadvcontroller_ns.class_('BleAdvEntity', cg.Component)
 
 FanLampEncoderV1 = bleadvcontroller_ns.class_('FanLampEncoderV1')
 FanLampEncoderV2 = bleadvcontroller_ns.class_('FanLampEncoderV2')
-ZhijiaEncoderV0 = bleadvcontroller_ns.class_('ZhijiaEncoderV0')
-ZhijiaEncoderV1 = bleadvcontroller_ns.class_('ZhijiaEncoderV1')
-ZhijiaEncoderV2 = bleadvcontroller_ns.class_('ZhijiaEncoderV2')
 
 BLE_ADV_ENCODERS = {
-    "fanlamp_pro" :{
+    "fanlamp_pro": {
         "variants": {
             "v1": {
                 "class": FanLampEncoderV1,
-                "args": [ 0x83, False ],
+                "args": [0x83, False],
                 "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x19, 0x03 ],
+                "ble_param": [0x19, 0x03],
                 "header": [0x77, 0xF8],
             },
             "v2": {
                 "class": FanLampEncoderV2,
-                "args": [ [0x10, 0x80, 0x00], 0x0400, False ],
-                "ble_param": [ 0x19, 0x03 ],
+                "args": [[0x10, 0x80, 0x00], 0x0400, False],
+                "ble_param": [0x19, 0x03],
                 "header": [0xF0, 0x08],
             },
             "v3": {
                 "class": FanLampEncoderV2,
-                "args": [ [0x20, 0x80, 0x00], 0x0400, True ],
-                "ble_param": [ 0x19, 0x03 ],
+                "args": [[0x20, 0x80, 0x00], 0x0400, True],
+                "ble_param": [0x19, 0x03],
                 "header": [0xF0, 0x08],
-            },
-            "v1a": {
-                "legacy": True,
-                "msg": "please use 'other - v1a' for exact replacement, or 'fanlamp_pro' v1 / v2 / v3 if effectively using FanLamp Pro app",
-            },
-            "v1b": {
-                "legacy": True,
-                "msg": "please use 'other - v1b' for exact replacement, or 'fanlamp_pro' v1 / v2 / v3 if effectively using FanLamp Pro app",
             },
         },
         "default_variant": "v3",
@@ -76,119 +63,25 @@ BLE_ADV_ENCODERS = {
         "variants": {
             "v1": {
                 "class": FanLampEncoderV1,
-                "args": [ 0x81 ],
+                "args": [0x81],
                 "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x19, 0x03 ],
+                "ble_param": [0x19, 0x03],
                 "header": [0x77, 0xF8],
             },
-            # v2 is only used by LampSmart Pro - Soft Lighting
             "v2": {
                 "class": FanLampEncoderV2,
-                "args": [ [0x10, 0x80, 0x00], 0x0100, False ],
-                "ble_param": [ 0x19, 0x03 ],
+                "args": [[0x10, 0x80, 0x00], 0x0100, False],
+                "ble_param": [0x19, 0x03],
                 "header": [0xF0, 0x08],
             },
             "v3": {
                 "class": FanLampEncoderV2,
-                "args": [ [0x30, 0x80, 0x00], 0x0100, True ],
-                "ble_param": [ 0x19, 0x03 ],
-                "header": [0xF0, 0x08],
-            },
-            "v1a": {
-                "legacy": True,
-                "msg": "please use 'other - v1a' for exact replacement, or 'lampsmart_pro' v1 / v3 if effectively using LampSmart Pro app",
-            },
-            "v1b": {
-                "legacy": True,
-                "msg": "please use 'other - v1b' for exact replacement, or 'lampsmart_pro' v1 / v3 if effectively using LampSmart Pro app",
-            },
-        },
-        "default_variant": "v3",
-        "default_forced_id": 0,
-    },
-    "zhijia": {
-        "variants": {
-            "v0": {
-                "class": ZhijiaEncoderV0,
-                "args": [],
-                "max_forced_id": 0xFFFF,
-                "ble_param": [ 0x1A, 0xFF ],
-                "header": [ 0xF9, 0x08, 0x49 ],
-            },
-            "v1": {
-                "class": ZhijiaEncoderV1,
-                "args": [],
-                "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x1A, 0xFF ],
-                "header": [ 0xF9, 0x08, 0x49 ],
-            },
-            "v2": {
-                "class": ZhijiaEncoderV2,
-                "args": [],
-                "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x1A, 0xFF ],
-                "header": [ 0x22, 0x9D ],
-            },
-        },
-        "default_variant": "v2",
-        "default_forced_id": 0xC630B8,
-    },
-    "remote" : {
-        "variants": {
-            "v1": {
-                "class": FanLampEncoderV1, 
-                "args": [ 0x83, False, True ],
-                "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x00, 0xFF ],
-                "header":[0x56, 0x55, 0x18, 0x87, 0x52], 
-            },
-            "v3": {
-                "class": FanLampEncoderV2,
-                "args": [ [0x10, 0x00, 0x56], 0x0400, True ],
-                "ble_param": [ 0x02, 0x16 ],
+                "args": [[0x30, 0x80, 0x00], 0x0100, True],
+                "ble_param": [0x19, 0x03],
                 "header": [0xF0, 0x08],
             },
         },
         "default_variant": "v3",
-        "default_forced_id": 0,
-    },
-# legacy lampsmart_pro variants v1a / v1b / v2 / v3
-# None of them are actually matching what FanLamp Pro / LampSmart Pro apps are generating
-# Maybe generated by some remotes, kept here for backward compatibility, with some raw sample
-    "other" : {
-        "variants": {
-            "v1b": {
-                "class": FanLampEncoderV1,
-                "args": [ 0x81, True, True, 0x55 ],
-                "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x02, 0x16 ],
-                "header":  [0xF9, 0x08],
-                # 02.01.02.1B.03.F9.08.49.13.F0.69.25.4E.31.51.BA.32.08.0A.24.CB.3B.7C.71.DC.8B.B8.97.08.D0.4C (31)
-            },
-            "v1a": {
-                "class": FanLampEncoderV1, 
-                "args": [ 0x81, True, True ],
-                "max_forced_id": 0xFFFFFF,
-                "ble_param": [ 0x02, 0x03 ],
-                "header": [0x77, 0xF8],
-                # 02.01.02.1B.03.77.F8.B6.5F.2B.5E.00.FC.31.51.50.CB.92.08.24.CB.BB.FC.14.C6.9E.B0.E9.EA.73.A4 (31)
-            },
-            "v2": {
-                "class": FanLampEncoderV2,
-                "args": [ [0x10, 0x80, 0x00], 0x0100, False ],
-                "ble_param": [ 0x19, 0x16 ],
-                "header": [0xF0, 0x08],
-                # 02.01.02.1B.16.F0.08.10.80.0B.9B.DA.CF.BE.B3.DD.56.3B.E9.1C.FC.27.A9.3A.A5.38.2D.3F.D4.6A.50 (31)
-            },
-            "v3": {
-                "class": FanLampEncoderV2,
-                "args": [ [0x10, 0x80, 0x00], 0x0100, True ],
-                "ble_param": [ 0x19, 0x16 ],
-                "header": [0xF0, 0x08],
-                # 02.01.02.1B.16.F0.08.10.80.33.BC.2E.B0.49.EA.58.76.C0.1D.99.5E.9C.D6.B8.0E.6E.14.2B.A5.30.A9 (31)
-            },
-        },
-        "default_variant": "v1b",
         "default_forced_id": 0,
     },
 }
@@ -211,36 +104,27 @@ CONTROLLER_BASE_CONFIG = cv.ENTITY_BASE_SCHEMA.extend(
     }
 )
 
-def validate_legacy_variant(config):
-    encoding = config[CONF_BLE_ADV_ENCODING]
-    variant = config[CONF_VARIANT]
-    pv = BLE_ADV_ENCODERS[ encoding ]["variants"][ variant ]
-    if pv.get("legacy", False):
-        raise cv.Invalid("DEPRECATED '%s - %s', %s" % (encoding, variant, pv["msg"]))
-    return config
-
 def validate_forced_id(config):
     encoding = config[CONF_BLE_ADV_ENCODING]
     variant = config[CONF_VARIANT]
     forced_id = config[CONF_BLE_ADV_FORCED_ID]
-    params = BLE_ADV_ENCODERS[ encoding ]
-    max_forced_id = params["variants"][ variant ].get("max_forced_id", 0xFFFFFFFF)
-    if forced_id > max_forced_id :
+    params = BLE_ADV_ENCODERS[encoding]
+    max_forced_id = params["variants"][variant].get("max_forced_id", 0xFFFFFFFF)
+    if forced_id > max_forced_id:
         raise cv.Invalid("Invalid 'forced_id' for %s - %s: %s. Maximum: 0x%X." % (encoding, variant, forced_id, max_forced_id))
     return config
 
 CONFIG_SCHEMA = cv.All(
     cv.Any(
-        *[ CONTROLLER_BASE_CONFIG.extend(
+        *[CONTROLLER_BASE_CONFIG.extend(
             {
                 cv.Required(CONF_BLE_ADV_ENCODING): cv.one_of(encoding),
                 cv.Optional(CONF_VARIANT, default=params["default_variant"]): cv.one_of(*params["variants"].keys()),
                 cv.Optional(CONF_BLE_ADV_FORCED_ID, default=params["default_forced_id"]): cv.hex_uint32_t,
             }
-        ) for encoding, params in BLE_ADV_ENCODERS.items() ]
+        ) for encoding, params in BLE_ADV_ENCODERS.items()]
     ),
     validate_forced_id,
-    validate_legacy_variant,
     cv.only_on([PLATFORM_ESP32]),
 )
 
@@ -251,27 +135,26 @@ async def entity_base_code_gen(var, config, platform):
 
 class BleAdvRegistry:
     handler = None
+
     @classmethod
-    def get(cls):
+    async def get(cls):
         if not cls.handler:
             hdl_id = ID("ble_adv_static_handler", type=BleAdvHandler)
             cls.handler = cg.new_Pvariable(hdl_id)
-            cg.add(cls.handler.set_component_source("ble_adv_handler"))
-            cg.add(cg.App.register_component(cls.handler))
+            await cg.register_component(cls.handler, {})
             for encoding, params in BLE_ADV_ENCODERS.items():
                 for variant, param_variant in params["variants"].items():
-                    if "class" in param_variant:
-                        enc_id = ID("enc_%s_%s" % (encoding, variant), type=param_variant["class"])
-                        enc = cg.new_Pvariable(enc_id, encoding, variant, *param_variant["args"])
-                        cg.add(enc.set_ble_param(*param_variant["ble_param"]))
-                        cg.add(enc.set_header(param_variant["header"]))
-                        cg.add(cls.handler.add_encoder(enc))
+                    enc_id = ID("enc_%s_%s" % (encoding, variant), type=param_variant["class"])
+                    enc = cg.new_Pvariable(enc_id, encoding, variant, *param_variant["args"])
+                    cg.add(enc.set_ble_param(*param_variant["ble_param"]))
+                    cg.add(enc.set_header(param_variant["header"]))
+                    cg.add(cls.handler.add_encoder(enc))
         return cls.handler
 
 async def to_code(config):
-    hdl = BleAdvRegistry.get()
+    hdl = await BleAdvRegistry.get()
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_setup_priority(300)) # start after Bluetooth
+    cg.add(var.set_setup_priority(300))
     await cg.register_component(var, config)
     await setup_entity(var, config, "ble_adv_controller")
     cg.add(var.set_handler(hdl))
@@ -285,5 +168,3 @@ async def to_code(config):
     else:
         cg.add(var.set_forced_id(config[CONF_ID].id))
     cg.add(var.set_show_config(config[CONF_BLE_ADV_SHOW_CONFIG]))
-
-
